82b98f2d2ce0e1488fb1bc7b4a9717aaca6a8dd4
Atozcartoonist987 <87314877+Atozcartoonist987@users.noreply.github.com>
2021-11-10 09:26:49 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,21 +8,21 @@ import os
 
 
 class Config(object):
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	API_ID = int(os.environ.get("API_ID", 12345))
-	API_HASH = os.environ.get("API_HASH")
-	STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "NoNeed")
-	STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME", "NoNeed")
-	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
-	DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
-	PRESET = os.environ.get("PRESET", "ultrafast")
-	OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
+	BOT_TOKEN = "2058040826:AAHSqEaRW-TOMwJXtZKSeEV9wlGmXu2ZkNI"
+	API_ID = "8755370"
+	API_HASH = "54261233493bbc5bbf489146dd2909dc"
+	STREAMTAPE_API_PASS = "Noneeded"
+	STREAMTAPE_API_USERNAME = "Noneeded"
+	LOG_CHANNEL = "1001651214398"
+	UPDATES_CHANNEL = ""
+	DOWN_PATH = "./downloads"
+	PRESET = "PRESET", "ultrafast"
+	OWNER_ID = ""
 	CAPTION = "By @AHToolsBot"
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "VideoWatermark_Bot")
-	DATABASE_URL = os.environ.get("DATABASE_URL")
-	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-	ALLOW_UPLOAD_TO_STREAMTAPE = bool(os.environ.get("ALLOW_UPLOAD_TO_STREAMTAPE", True))
+	BOT_USERNAME = "akwatermark_bot"
+	DATABASE_URL = "mongodb+srv://atozcartoonist:Pubghacker@987@cluster0.kua5r.mongodb.net/cluster0?retryWrites=true&w=majority" 
+	BROADCAST_AS_COPY = "False"
+	ALLOW_UPLOAD_TO_STREAMTAPE = "True"
 	USAGE_WATERMARK_ADDER = """
 Hi, I am Video Watermark Adder Bot!
 
